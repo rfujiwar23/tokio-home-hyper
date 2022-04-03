7e1507fbd2c654b7c7e605f00f73d926cbbd14d6
Ryo Fujiwara <jjcproject@icloud.com>
2022-04-03 23:21:39 +0900

    more to add tomorrow

---
+++ assets/js/section09.js
@@ -0,0 +1,12 @@
+
+
+
+  new Vue({
+    el: '#section09',
+    data: {
+        salonType: [
+            { img: 'assets/img/img-kamisma.png', text: 'KAMISMA BEAUTY' },
+            { img: 'assets/img/img-tokio.png', text: 'TOKIO' },
+          ]
+    }
+  });
